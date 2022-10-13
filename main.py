e0f746777a33729aff02f390ad7cf34ddd05500a
Victor Ponce <vicmanuelr@gmail.com>
2022-10-13 17:43:31 -0600

    edit parameters for each type of request moved to function
    - created post pixel that will post new entry especified

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import datetime as dt
 
 PIXELA_HEADER ={
     "X-USER-TOKEN": os.environ.get("PASSWORD")
@@ -7,32 +8,46 @@ PIXELA_HEADER ={
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 USERNAME = "vicmanuelr"
 PASSWORD = os.environ.get("PASSWORD")
-GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
-CREATE_USER_PARAMS = {
-    "token": PASSWORD,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
-}
-GRAPH_PARAMS = {
-    "id": "graph1",
-    "name": "Coding Graph",
-    "unit": "minutes",
-    "type": "float",
-    "color": "shibafu"
-}
+GRAPH_ID = "graph1"
 
-
-def create_user(user_parameters: dict):
-    response = requests.post(url=PIXELA_ENDPOINT, json=user_parameters)
+def create_user():
+    create_user_params = {
+        "token": PASSWORD,
+        "username": USERNAME,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes",
+    }
+    response = requests.post(url=PIXELA_ENDPOINT, json=create_user_params)
     print(response.text)
 
 
 # create_user(CREATE_USER_PARAMS)
 
-def create_graph(graph_parameters: dict, auth_headers: dict):
-    response = requests.post(url=GRAPH_ENDPOINT, headers=auth_headers, json=graph_parameters)
+
+def create_graph(auth_headers: dict):
+    graph_params = {
+        "id": "graph1",
+        "name": "Coding Graph",
+        "unit": "minutes",
+        "type": "float",
+        "color": "shibafu"
+    }
+    graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+    response = requests.post(url=graph_endpoint, headers=auth_headers, json=graph_params)
     print(response.text)
 
 
-# create_graph(GRAPH_PARAMS, PIXELA_HEADER)
+# create_graph(graph_params, PIXELA_HEADER)
+
+
+def post_pixel(auth_headers: dict, quantity: int):
+    post_pixel_params = {
+        "date": dt.date.today().strftime("%Y%m%d"),
+        "quantity": f"{quantity}",
+    }
+    post_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+    response = requests.post(url=post_endpoint, headers=auth_headers, json=post_pixel_params)
+    print(response.text)
+
+
+post_pixel(PIXELA_HEADER, 50)
