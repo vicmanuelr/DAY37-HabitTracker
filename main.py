ff5d04ff53ef32b96cb35853e22966df62f0c623
Victor Ponce <vicmanuelr@gmail.com>
2022-10-13 12:50:04 -0600

    Created the coding graph
    - create graph requires 2 parameters that will be the authentication
      headers and the graph parameters
    - the endpoint of the conding graph also change especified as a constant

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import requests
+import os
 
+PIXELA_HEADER ={
+    "X-USER-TOKEN": os.environ.get("PASSWORD")
+}
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
-USER_PARAMS = {
-    "token": "Google@2018",
-    "username": "vicmanuelr",
-    "agreeTermsOfService": "Yes",
-    "notMinor": "Yes",
+USERNAME = "vicmanuelr"
+PASSWORD = os.environ.get("PASSWORD")
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+CREATE_USER_PARAMS = {
+    "token": PASSWORD,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
 }
-HEADER ={
-    "X-USER-TOKEN": "Google@2018"
+GRAPH_PARAMS = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "minutes",
+    "type": "float",
+    "color": "shibafu"
 }
 
-requests.post(url=PIXELA_ENDPOINT, params=USER_PARAMS)
+
+def create_user(user_parameters: dict):
+    response = requests.post(url=PIXELA_ENDPOINT, json=user_parameters)
+    print(response.text)
+
+
+# create_user(CREATE_USER_PARAMS)
+
+def create_graph(graph_parameters: dict, auth_headers: dict):
+    response = requests.post(url=GRAPH_ENDPOINT, headers=auth_headers, json=graph_parameters)
+    print(response.text)
+
+
+# create_graph(GRAPH_PARAMS, PIXELA_HEADER)
