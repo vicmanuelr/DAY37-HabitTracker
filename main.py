e242638761e3d771d8296471bff1cd641788749d
Victor Ponce <vicmanuelr@gmail.com>
2022-10-13 11:33:24 -0600

    User creation on pixela

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import requests
 
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+USER_PARAMS = {
+    "token": "Google@2018",
+    "username": "vicmanuelr",
+    "agreeTermsOfService": "Yes",
+    "notMinor": "Yes",
+}
+HEADER ={
+    "X-USER-TOKEN": "Google@2018"
+}
+
+requests.post(url=PIXELA_ENDPOINT, params=USER_PARAMS)
